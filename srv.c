3c052387166c40543a71cf108638e85b2a45e481
SLITES <slites@yandex.ru>
2013-11-11 14:48:38 +0400

    Add shared memory

--- srv.c
+++ srv.c
@@ -5,27 +5,26 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-
 void sendBuf(char *message,int size, int client_fd)
 {
     int total = 0;
-    int n;
-printf("Client FD %d \n", client_fd);
+    int n=0;
+    printf("Client FD %d \n", client_fd);
     while(total < size)
     {
         n = send(client_fd,(message+n), size-n,0);
-        if(n == -1)
+	printf("Result %d,  \n", n);        
+	if(n == -1)
             break;
         total += n;
-	printf("Byte %d \n", total);
     }
 }
 #define TRACE printf("%s; %d\n", __FILE__, __LINE__);
 int main()
 {
-    printf("Good\n");
-    char message[] = "HTTP/1.1 200 OK\r\n Server: YarServer/2009-09-09\r\n Content-Type: text/html\r\n Content-Length: 43\r\n Connection: close\r\n\r\n <html><body>сервер работает</body></html>\n";    
-int server_fd, server_ln;
+    printf("Server wait\n");
+    char header[] = "HTTP/1.1 200 OK\r\n\n";    
+	int server_fd, server_ln;
     int client_fd, client_ln;
     int bind_s, list_s;
     struct sockaddr_in server_addres;
@@ -36,17 +35,15 @@ int server_fd, server_ln;
     server_addres.sin_port = htons(2235);
     server_ln = sizeof(server_addres);
     bind_s = bind(server_fd, (struct sockaddr *) &server_addres, server_ln);
- list_s = listen(server_fd,5);
+	list_s = listen(server_fd,5);
 TRACE
     printf("Bind %d, List %d \n",bind_s, list_s);
     while(1)
     {
-        client_ln = sizeof(client_addres);
-client_fd = accept(server_fd, (struct sockaddr *) &client_addres, &client_ln);
-	TRACE        
-	//printf("Client FD %d",client_fd);
-	//write(client_fd,(char *)&message,1);
-	sendBuf(&message[0],sizeof(message),client_fd);
+		client_ln = sizeof(client_addres);
+		client_fd = accept(server_fd, (struct sockaddr *) &client_addres, &client_ln);
+		TRACE        
+		sendBuf(&header[0],sizeof(header),client_fd);
     }
     return 0;
 }
